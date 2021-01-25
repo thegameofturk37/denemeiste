5b1b8052b0ff8ec61b604489d441d3aad5a6c897
berke <73236722+thegameofturk37@users.noreply.github.com>
2021-01-25 12:20:07 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+    "ownerID": "446048541488578569", //kendi IDınızı yazınız
+    "admins": ["446048541488578569"],
+    "support": ["446048541488578569"],
+    "token": "NDg1MTEwODUyNzM5OTIzOTY5.XWXoOA.R7fx41HJF9y-JMHWJvxNndZOPzk", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "lNYupwJAzgSjqLSgnf8z5zZITsalw2K4", //botunuzun secretini yazınız
+      "callbackURL": `https://Neptunebot-gecilcek.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "xyzxyz", //kalsın
+      "domain": "https://Neptunebot-gecilcek.glitch.me/" //site URLnizi yazınız!
+    }
+  };
+  
+  module.exports = config;
